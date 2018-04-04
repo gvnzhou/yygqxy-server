65e1c42c4dd88b8d4d1ef5995f4fca2bcda3a9a4
javion25 <228806684@qq.com>
2018-04-04 19:00:55 +0800

    update

--- controllers/song.js
+++ controllers/song.js
@@ -1,16 +1,19 @@
 // const errorMsg = require('../core/errorMsg');
 // const model = require('../models/model');
 // const md5 = require('js-md5');
+const db = require('../lib/mongodb');
+const assert = require('assert');
 
 const SongController = {};
 
-SongController.getSongList = async function (ctx) {
+SongController.getSongList = async function (ctx, next) {
 	
-	const regType = ctx.request.body.regType;
-	ctx.body = {
-		a: 111,
-		b: 222
-	}
+	// 查询数据库 获取数据
+	
+	// const regType = ctx.request.body.regType;
+	// ctx.body = await db.c('song');
+	const res = await db.c('song').find({}).limit(3);
+	ctx.body = res.toString();
 }
 
 SongController.getSongDetail = async function (ctx) {
